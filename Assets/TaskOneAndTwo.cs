adb6e3e92f65a1ae390208ce2f70e44a35883fde
Cami Alarcon-Fernandez <camialarconfern@gmail.com>
2026-04-05 17:50:27 -0400

    Completed tasks 1 and 2 in TaskOneAndTwo

--- Assets/TaskOneAndTwo.cs
+++ Assets/TaskOneAndTwo.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 
 public class TaskOneAndTwo : MonoBehaviour
 {
-
+    // Task 1 and 2 variables and helper functions
+    
+    // TASK 1
     public static string[] firstNames =
     {
         "Chandler",
@@ -49,28 +51,135 @@ public class TaskOneAndTwo : MonoBehaviour
         return randomName;
     }
 
+    public bool QueueIsEmpty()
+    {
+        return loginQueue.Count == 0;
+    }
+
+
+    // TASK 2
+    public string[] randomPlayerNames = new string[15];
+    public HashSet<string> seen = new HashSet<string>();
+    public HashSet<string> duplicates = new HashSet<string>();
+    public void GetRandomFirstNames()
+    {
+        for(int i = 0; i<randomPlayerNames.Length; i++)
+        {
+            randomPlayerNames[i] = firstNames[GetRandomNum()];
+        }
+        string playerNames = "Created the name array: ";
+        foreach (var name in randomPlayerNames)
+        {
+            playerNames += $"{name}, ";
+        }
+        playerNames = playerNames.TrimEnd(',', ' ');
+        Debug.Log(playerNames);
+    }
+
+
 
     // Start is called before the first frame update
     void Start()
     {
-        //GetRandomPlayerName();
-        for(int i = 0; i<6; i++)
+        // enqueue 4-6 players to the login queue
+        for (int i = 0; i<UnityEngine.Random.Range(4, 7); i++)
         {
             loginQueue.Enqueue(GetRandomPlayerName());
         }
+        
+
+        // Start the tasks
+        StartCoroutine(TaskManager());
+    }
+
+
+
+    IEnumerator TaskManager()
+    {
+        Debug.Log("TASK 1:");
         var playerListQuery = from name in loginQueue
                               select name;
         List<string> playerList = playerListQuery.ToList();
-        foreach(var name in playerList)
-        {
-            //Debug.LogFormat(""
-        }
 
+        // print initial state
+        string initialState = $"Initial login queue created. There are {playerList.Count} players in the queue: ";
+        foreach (var name in playerList)
+        {
+            // if time come back to remove last comma and space
+            initialState += $"{name}., ";
+        }
+        initialState = initialState.TrimEnd(',', ' ');
+        Debug.Log(initialState);
+
+        Coroutine addPlayer = StartCoroutine(AddPlayer());
+        Coroutine loginPlayer = StartCoroutine(LoginPlayer());
+        yield return addPlayer;
+        yield return loginPlayer;
+
+
+        Debug.Log("TASK 2:");
+        DetectDuplicateNames();
+        yield return null;
     }
 
-    // Update is called once per frame
-    void Update()
+    // TASK 1
+    IEnumerator AddPlayer()
     {
-        
+        while(!QueueIsEmpty())
+        {
+            yield return new WaitForSeconds(4);
+            if(QueueIsEmpty())
+            {
+                yield break; // Exit the coroutine if the queue is empty
+            }
+            string newPlayer = GetRandomPlayerName();
+            loginQueue.Enqueue(newPlayer);
+            Debug.Log($"{newPlayer}. is trying to login and added to the login queue.");
+        }
+        yield return null;
+    }
+
+    IEnumerator LoginPlayer()
+    {
+        while (!QueueIsEmpty())
+        {
+            yield return new WaitForSeconds(3);
+            if (QueueIsEmpty())
+            {
+                yield break; // Exit the coroutine if the queue is empty
+            }
+            string playerLoggingIn = loginQueue.Dequeue();
+            Debug.Log($"{playerLoggingIn}. is now inside the game.");
+        }
+        Debug.Log("Login server is idle. No players are waiting.");
+        yield return null;
+    }
+
+
+    // TASK 2
+    public void DetectDuplicateNames()
+    {
+        // create list of names and search for duplicates
+        GetRandomFirstNames();
+        for (int i = 0; i < randomPlayerNames.Length; i++)
+        {
+            if (!seen.Add(randomPlayerNames[i]))
+            {
+                duplicates.Add(randomPlayerNames[i]);
+            }
+        }
+
+        // print duplicate names
+        if(duplicates.Count() > 0)
+        {
+            string duplicateNames = string.Join(", ", duplicates);
+            string result = "The array has duplicate names: " + duplicateNames;
+            Debug.Log(result);
+
+        }
+        else
+        {
+            Debug.Log("The array has no duplicate names.");
+        }
     }
 }
